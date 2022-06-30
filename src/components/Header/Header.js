d144d69ac2914fabecdb11378de76dbfb42e40b4
Derek Reilly <derekreilly1990@gmail.com>
2022-06-30 09:27:25 +0200

    feat: adjust header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <SubscribeAction>
+          <Button>
+            Subscribe
+          </Button>
+          <SubscriberLink>Already a subscriber</SubscriberLink>
+        </SubscribeAction>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,12 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+  
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -58,6 +78,36 @@ const ActionGroup = styled.div`
     display: block;
   }
 `;
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+  
+`
+const SubscribeAction = styled.div`
+  display: none;
+  flex: 1;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+    position: relative;
+    justify-self: end;
+  }
+  
+`
+const SubscriberLink = styled.a`
+  font-size: 0.75rem;
+  color: var(--color-gray-900);
+  font-family: var(--font-family-sans-serif);
+  font-weight: var(--font-weight-bold);
+
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 2px;
+  }
+`;
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
@@ -65,6 +115,20 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+    justify-items: start;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
